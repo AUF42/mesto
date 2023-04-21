efd0049d8cdbecfcc16569ce49c082724ed930b2
AUF42 <fr95juli@gmail.com>
2023-04-21 18:33:02 +0200

    fixies after review

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
 const popupEditProfile = document.querySelector('.profile__edit');
 const popup = document.querySelector('.popup');
 const popupCloseBtn = popup.querySelector('.popup__close-button');
+const popupForm = popup.querySelector('.popup__form')
 
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__caption');
@@ -8,19 +9,23 @@ let inputName = document.querySelector('#input-name');
 let inputJob = document.querySelector('#input-profile-caption');
 
 
-const popupToggle = function () {
-    popup.classList.toggle('popup__open');
+const popupOpen = function () {
+    popup.classList.add('popup__opened');
     inputName.value = profileName.textContent;
     inputJob.value = profileJob.textContent;
+}
+
+const popupClose = function () {
+    popup.classList.remove('popup__opened');
 }
 
 const formSubmitHandler = function (evt) {
     evt.preventDefault();
     profileName.textContent = inputName.value;
     profileJob.textContent = inputJob.value;
-    popupToggle();
+    popupClose();
 }
 
-popupEditProfile.addEventListener('click', popupToggle);
-popupCloseBtn.addEventListener('click', popupToggle);
-popupEditProfile.addEventListener('submit', formSubmitHandler);
+popupEditProfile.addEventListener('click', popupOpen);
+popupCloseBtn.addEventListener('click', popupClose);
+popupForm.addEventListener('submit', formSubmitHandler);
